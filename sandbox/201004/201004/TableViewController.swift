f393548230054131e55bcc0b41cb1b53ce7206e9
KIM TAE HEON <danmujicafe@gmail.com>
2020-10-04 12:48:35 +0900

    updated_array_디테일뷰컨트롤러

--- sandbox/201004/201004/TableViewController.swift
+++ sandbox/201004/201004/TableViewController.swift
@@ -14,17 +14,21 @@ class TableViewController: UITableViewController {
         let title : String
         let year : Int
     }
+
     
-    let movie1 = Movie(title: "테넷", year: 2020)
-    let movie2 = Movie(title: "덩케르크", year: 2017)
-    let movie3 = Movie(title: "인터스텔라", year: 2014)
-    let movie4 = Movie(title: "다크나이트 라이즈", year: 2012)
-    let movie5 = Movie(title: "인셉션", year: 2010)
-    let movie6 = Movie(title: "다크나이트", year: 2008)
-    let movie7 = Movie(title: "프레스티지", year: 2006)
-    let movie8 = Movie(title: "배트맨 비긴즈", year: 2005)
-    let movie9 = Movie(title: "인썸니아", year: 2002)
-    let movie10 = Movie(title: "메멘토", year: 2000)
+    let MovieList = [
+        Movie(title: "테넷", year: 2020),
+        Movie(title: "덩케르크", year: 2017),
+        Movie(title: "인터스텔라", year: 2014),
+        Movie(title: "다크나이트 라이즈", year: 2012),
+        Movie(title: "인셉션", year: 2010),
+        Movie(title: "다크나이트", year: 2008),
+        Movie(title: "프레스티지", year: 2006),
+        Movie(title: "배트맨 비긴즈", year: 2005),
+        Movie(title: "인썸니아", year: 2002),
+        Movie(title: "메멘토", year: 2000)
+            
+    ]
     
 
     override func viewDidLoad() {
@@ -41,14 +45,32 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 10
+        return MovieList.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let item = MovieList[indexPath.row]
+        cell.textLabel?.text = item.title
     // 개별 셀에 내용 채우기
     // 기본셀 : textLabel, detailTextLabel, imageView
-        cell.textLabel?.text = "\(indexPath.row)"
+//        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
         
-    return cell }
+    }
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? DetailViewController{
+//            vc.data = "Hello"
+            if let cell = sender as? UITableViewCell,
+               let indexPath = tableView.indexPath(for: cell){
+                let selected = MovieList[indexPath.row]
+               vc.data = selected.title
+//                vc.data = selected
+             }
+        }
+        
+        
+    }
 
 }
